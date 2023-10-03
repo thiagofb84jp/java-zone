1cf6123c53950ed9f144835cda9969475f3553c7
Thiago Ferreira <thiagofb84jp@gmail.com>
2023-10-03 12:14:25 -0300

    Including a new exercise about structure decision

---
+++ java-zone/src/structureDecision/Exercise01.java
@@ -0,0 +1,13 @@
+package structureDecision;
+
+import java.util.Scanner;
+
+public class Exercise01 {
+    /***
+     * 1. Faça um Programa que peça dois números e imprima o maior deles.
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+    }
+}
