b71df772742bdaa2ec0079440e26f4dbe44f785e
Thiago Ferreira <thiagofb84jp@gmail.com>
2023-10-20 11:34:47 -0300

    Including more tests

---
+++ java-zone/src/arrayList/Exercise19.java
@@ -0,0 +1,10 @@
+package arrayList;
+
+public class Exercise19 {
+    /**
+     * 19. Write a Java program to copy one array list into another
+     */
+    public static void main(String[] args) {
+        
+    }
+}
